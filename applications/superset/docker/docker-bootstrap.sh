a2b80ae2ac1d12206fbbb04d001e34e89d96e716
Fabricio Vital da Silva <fabriciovitaljobs@gmail.com>
2024-12-19 01:21:49 +0000

    Ajustando

--- applications/superset/docker/docker-bootstrap.sh
+++ applications/superset/docker/docker-bootstrap.sh
@@ -18,6 +18,11 @@
 
 set -eo pipefail
 
+# Make python interactive
+if [ "$DEV_MODE" == "true" ]; then
+    echo "Reinstalling the app in editable mode"
+    uv pip install -e .
+fi
 REQUIREMENTS_LOCAL="/app/docker/requirements-local.txt"
 # If Cypress run – overwrite the password for admin and export env variables
 if [ "$CYPRESS_CONFIG" == "true" ]; then
@@ -25,26 +30,40 @@ if [ "$CYPRESS_CONFIG" == "true" ]; then
     export SUPERSET_TESTENV=true
     export SUPERSET__SQLALCHEMY_DATABASE_URI=postgresql+psycopg2://superset:superset@db:5432/superset
 fi
+if [[ "$DATABASE_DIALECT" == postgres* ]] ; then
+    echo "Installing postgres requirements"
+    uv pip install -e .[postgres]
+fi
 #
 # Make sure we have dev requirements installed
 #
 if [ -f "${REQUIREMENTS_LOCAL}" ]; then
   echo "Installing local overrides at ${REQUIREMENTS_LOCAL}"
-  pip install -r "${REQUIREMENTS_LOCAL}"
+  uv pip install --no-cache-dir -r "${REQUIREMENTS_LOCAL}"
 else
   echo "Skipping local overrides"
 fi
 
-if [[ "${1}" == "worker" ]]; then
-  echo "Starting Celery worker..."
-  celery --app=superset.tasks.celery_app:app worker -Ofair -l INFO
-elif [[ "${1}" == "beat" ]]; then
-  echo "Starting Celery beat..."
-  celery --app=superset.tasks.celery_app:app beat --pidfile /tmp/celerybeat.pid -l INFO -s "${SUPERSET_HOME}"/celerybeat-schedule
-elif [[ "${1}" == "app" ]]; then
-  echo "Starting web app..."
-  flask run -p 8088 --with-threads --reload --debugger --host=0.0.0.0
-elif [[ "${1}" == "app-gunicorn" ]]; then
-  echo "Starting web app..."
-  /usr/bin/run-server.sh
-fi
+case "${1}" in
+  worker)
+    echo "Starting Celery worker..."
+    # setting up only 2 workers by default to contain memory usage in dev environments
+    celery --app=superset.tasks.celery_app:app worker -O fair -l INFO --concurrency=${CELERYD_CONCURRENCY:-2}
+    ;;
+  beat)
+    echo "Starting Celery beat..."
+    rm -f /tmp/celerybeat.pid
+    celery --app=superset.tasks.celery_app:app beat --pidfile /tmp/celerybeat.pid -l INFO -s "${SUPERSET_HOME}"/celerybeat-schedule
+    ;;
+  app)
+    echo "Starting web app (using development server)..."
+    flask run -p 8088 --with-threads --reload --debugger --host=0.0.0.0
+    ;;
+  app-gunicorn)
+    echo "Starting web app..."
+    /usr/bin/run-server.sh
+    ;;
+  *)
+    echo "Unknown Operation!!!"
+    ;;
+esac
